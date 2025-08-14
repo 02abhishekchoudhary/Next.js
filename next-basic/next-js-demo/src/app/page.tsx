b491dcca2694f2d7052b9ba7a146fc1b5b356231
Abhishek Choudhary <abhishekchoudharysc@gmail.com>
2025-08-14 23:56:25 +0530

    File base routing

--- next-basic/next-js-demo/src/app/page.tsx
+++ next-basic/next-js-demo/src/app/page.tsx
@@ -24,7 +24,7 @@ export default function Home() {
             </code>
             .
           </li>
-          <li className="tracking-[-.01em]">Hello World</li>
+          <li className="tracking-[-.01em]">Hello World!</li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
